3124adc46fa9fac64a991397c5c2c1c10d282c50
Nourhan <nmohamed@tendegrees.sa>
2021-01-29 21:38:19 +0200

    - Add tests for check subscription status request.

---
+++ tests/Feature/SubscriptionStatusTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SubscriptionStatusTest extends TestCase
+{
+    use RefreshDatabase, ArraySubsetAsserts;
+
+
+    /** @test */
+    public function subscription_status_checked_successfully()
+    {
+        $data = [
+            'uid' => '5213-5423-hj5uj-5jhg',
+            'app_id' => '1234568',
+            'language' => 'english',
+            'operating_system' => 'android'
+        ];
+
+        $deviceResponse = $this->json('POST', route('app.register'), $data);
+        $token = $deviceResponse->json()['data']['token'];
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+            'Accept' => 'application/json'
+        ])->json('POST', route('app.purchase'), ['receipt' => '1231']);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+            'Accept' => 'application/json'
+        ])->json('GET', route('app.check.subscription'));
+
+        $response->assertStatus(200);
+        $response->assertSee('active');
+    }
+
+
+    /** @test */
+    public function no_subscription_found()
+    {
+        $data = [
+            'uid' => '5213-5423-hj5uj-5jhg',
+            'app_id' => '1234568',
+            'language' => 'english',
+            'operating_system' => 'android'
+        ];
+
+        $deviceResponse = $this->json('POST', route('app.register'), $data);
+        $token = $deviceResponse->json()['data']['token'];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+            'Accept' => 'application/json'
+        ])->json('GET', route('app.check.subscription'));
+
+        $response->assertStatus(406);
+        $this->assertArraySubset(['message' => __('No subscriptions existing for this app!')], $response->json());
+    }
+
+    /** @test */
+    public function not_authorized_device_cant_check_status()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->json('GET', route('app.check.subscription'));
+
+        $response->assertStatus(401);
+        $this->assertArraySubset(['message' => 'Unauthenticated.'], $response->json());
+    }
+}
